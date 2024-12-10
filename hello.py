bf5255cf3a0521e3bb43ab98d4ce19e8d54ee0c9
Azizbekdevn1 <azizbekmurodov2003@gmail.com>
2024-12-10 13:11:16 +0500

    Add date

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 username = input("Please, enter your username: ")
 print(f"Hi, {username}! Welcome!")
 print("We are learning Git!")
 print("Another line!")
 print("Now, we learning GitHub!")
+print("Today:", datetime.today().strftime('%d/%m/%Y'))
